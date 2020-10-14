0e5a3dcbb7f278f2c9cb8aa7436a6456aaceb642
DalatCoder <hieuntctk42@gmail.com>
2020-10-14 20:06:36 +0700

    Fade animation

---
+++ TuHoc/02.animations/app/src/main/java/com/example/animations/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.animations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean isCat1Showing = true;
+
+    public void fade(View view) {
+        ImageView imageViewCat1 = (ImageView) findViewById(R.id.imageViewCat1);
+        ImageView imageViewCat2 = (ImageView) findViewById(R.id.imageViewCat2);
+
+        if (isCat1Showing) {
+            isCat1Showing = false;
+            imageViewCat1.animate().alpha(0).setDuration(2000);
+            imageViewCat2.animate().alpha(1).setDuration(2000);
+        }
+        else {
+            isCat1Showing = true;
+            imageViewCat1.animate().alpha(1).setDuration(2000);
+            imageViewCat2.animate().alpha(0).setDuration(2000);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
