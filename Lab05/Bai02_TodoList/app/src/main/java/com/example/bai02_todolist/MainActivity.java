854d7a3af5d639cbfb84341fe2b1d5253d47244c
Trong Hieu <hieuntctk42@gmail.com>
2020-12-12 10:56:29 +0700

    Them cong viec moi 🐶🐶🐶

--- Lab05/Bai02_TodoList/app/src/main/java/com/example/bai02_todolist/MainActivity.java
+++ Lab05/Bai02_TodoList/app/src/main/java/com/example/bai02_todolist/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.bai02_todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -38,6 +47,12 @@ public class MainActivity extends AppCompatActivity {
         // Insert data
         // databaseHelper.QueryData("INSERT INTO CongViec(TenCV) VALUES ('Làm bài tập android')");
 
+        GetDataCongViec();
+    }
+
+    private void GetDataCongViec() {
+        arrayCongViec.clear();
+
         // Select data
         Cursor dataCongViec = databaseHelper.GetData("SELECT * FROM CongViec");
         while (true) {
@@ -52,5 +67,57 @@ public class MainActivity extends AppCompatActivity {
         }
 
         adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_congviec, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menuAdd) {
+            DialogThem();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void DialogThem() {
+        Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_them_cong_viec);
+
+        EditText edtTen = (EditText) dialog.findViewById(R.id.edittextTenCV);
+        Button btnThem = (Button) dialog.findViewById(R.id.buttonThemCV);
+        Button btnThoat = (Button) dialog.findViewById(R.id.buttonThoat);
+
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String tenCV = edtTen.getText().toString();
+
+                if (tenCV.length() == 0) {
+                    Toast.makeText(MainActivity.this, "Vui lòng nhập tên công việc", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                databaseHelper.QueryData("INSERT INTO CongViec(TenCV) VALUES ('" + tenCV + "')");
+                Toast.makeText(MainActivity.this, "Thêm công việc thành công", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+
+                GetDataCongViec();
+            }
+        });
+
+        btnThoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
 }
