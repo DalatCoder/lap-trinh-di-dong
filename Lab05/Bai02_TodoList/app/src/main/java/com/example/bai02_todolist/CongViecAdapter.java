95559f70f2f3d97f4ffa7c0ca117bae05d16c71e
Trong Hieu <hieuntctk42@gmail.com>
2020-12-12 10:18:14 +0700

    Render list cong viec 🚀🚀🚀

---
+++ Lab05/Bai02_TodoList/app/src/main/java/com/example/bai02_todolist/CongViecAdapter.java
@@ -0,0 +1,67 @@
+package com.example.bai02_todolist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CongViecAdapter extends BaseAdapter {
+
+    private Context context;
+    private int layout;
+    private List<CongViec> congViecList;
+
+    public CongViecAdapter(Context context, int layout, List<CongViec> congViecList) {
+        this.context = context;
+        this.layout = layout;
+        this.congViecList = congViecList;
+    }
+
+    @Override
+    public int getCount() {
+        return congViecList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    private class ViewHolder {
+        TextView txtTen;
+        ImageView imgDelete, imgEdit;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            holder = new ViewHolder();
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(layout, null);
+            holder.txtTen       = (TextView) convertView.findViewById(R.id.textviewTen);
+            holder.imgDelete    = (ImageView) convertView.findViewById(R.id.imageviewDelete);
+            holder.imgEdit      = (ImageView) convertView.findViewById(R.id.imageviewEdit);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        CongViec congViec = congViecList.get(position);
+
+        holder.txtTen.setText(congViec.getTen());
+
+        return convertView;
+    }
+}
--- Lab05/Bai02_TodoList/app/src/main/java/com/example/bai02_todolist/MainActivity.java
+++ Lab05/Bai02_TodoList/app/src/main/java/com/example/bai02_todolist/MainActivity.java
@@ -4,17 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper databaseHelper;
+
+    ListView lvCongViec;
+    ArrayList<CongViec> arrayCongViec;
+    CongViecAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        lvCongViec = (ListView) findViewById(R.id.listviewCongViec);
+        arrayCongViec = new ArrayList<CongViec>();
+
+        adapter = new CongViecAdapter(this, R.layout.dong_cong_viec, arrayCongViec);
+        lvCongViec.setAdapter(adapter);
 
         // Tạo database với tên GhiChu
         databaseHelper = new DatabaseHelper(this, "ghichu.sqlite", null, 1);
@@ -33,8 +46,11 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
             // Id: 0, TenCV: 1
+            int id = dataCongViec.getInt(0);
             String ten = dataCongViec.getString(1);
-            Toast.makeText(this, ten, Toast.LENGTH_LONG).show();
+            arrayCongViec.add(new CongViec(id, ten));
         }
+
+        adapter.notifyDataSetChanged();
     }
 }
